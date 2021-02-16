8b4bf736dd874373ce4877f10d24bf482d9421f1
Victor Goecking <victorgoecking96@gmail.com>
2021-02-16 18:36:57 -0300

    New Home

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,23 @@ function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Counter />
     </div>
   )
 }
 
-function Counter() {
-  const [counter, setCounter] = useState(1)
+// function Counter() {
+//   const [counter, setCounter] = useState(1)
 
-  function addCounter() {
-    setCounter(counter + 1)
-  }
+//   function addCounter() {
+//     setCounter(counter + 1)
+//   }
 
-  return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={addCounter}>Add</button>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//       <button onClick={addCounter}>Add</button>
+//     </div>
+//   )
+// }
 
 export default Home
