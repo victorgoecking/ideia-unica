4a6faa7bb776f1d69df75cfdadce3450bfa33d63
Victor Goecking <victorgoecking96@gmail.com>
2021-02-17 12:43:22 -0300

    add api dog random and environment variable

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,15 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+
+    const environmentVariable = process.env.DOG_API_RANDOM;
     const dynamicDate = new Date();
 
+    const dogRandomResponse = await fetch(`https://dog.ceo${environmentVariable}`);
+    const dogRandomResponseJson = await dogRandomResponse.json();
+    const dog = dogRandomResponseJson.message;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        dog: dog
     })
 }
 
