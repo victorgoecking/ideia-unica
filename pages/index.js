12e14128a6eed8be89d759f217fe2d9404238ffe
Victor Goecking <victorgoecking96@gmail.com>
2021-02-17 11:25:34 -0300

    Add delay

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,15 @@
-// import { useState } from 'react';
 import Link from 'next/link'
 
 function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/sobre">
-        <a>Acessar página Sobre</a>
-      </Link>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Home</h1>
+            
+            <Link href="/sobre">
+                <a>Acessar página Sobre</a>
+            </Link>
+        </div>
+    )
 }
-
-// function Counter() {
-//   const [counter, setCounter] = useState(1)
-
-//   function addCounter() {
-//     setCounter(counter + 1)
-//   }
-
-//   return (
-//     <div>
-//       <div>{counter}</div>
-//       <button onClick={addCounter}>Add</button>
-//     </div>
-//   )
-// }
 
 export default Home
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,29 +1,14 @@
 import Link from 'next/link'
 
 function Sobre() {
-  return (
-    <div>
-      <h1>Sobre</h1>
-      <Link href="/">
-        <a>Acessar página Home</a>
-      </Link>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <Link href="/">
+                <a>Acessar página Home</a>
+            </Link>
+        </div>
+    )
 }
-
-// function Counter() {
-//   const [counter, setCounter] = useState(1)
-
-//   function addCounter() {
-//     setCounter(counter + 1)
-//   }
-
-//   return (
-//     <div>
-//       <div>{counter}</div>
-//       <button onClick={addCounter}>Add</button>
-//     </div>
-//   )
-// }
 
 export default Sobre
